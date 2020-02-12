d803f2e8dffc6b17bfa94386577552e896a1adfc
Alexandre Feio <alefeio@gmail.com>
2020-02-12 03:59:13 -0300

    Semana Omnistack10 Rocketseat contluída: backend + web + mobile

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
 const ParseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket')
 
 // funções do controller
 // index, show. store, update e destroy
@@ -44,6 +45,16 @@ module.exports = {
                     techs: techsArray,
                     location
                 })
+
+                // Filtrar as conexões que estão há até 10km de distância
+                // e que tenham pelo menos uma das tecnologias filtradas
+
+                const sendSocketMessageTo = findConnections(
+                    { latitude, longitude },
+                    techsArray
+                )
+
+                sendMessage(sendSocketMessageTo, 'new-dev', dev)
             }
 
             return res.json(dev)
