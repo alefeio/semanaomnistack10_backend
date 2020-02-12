f6d2536a89b2b4c598b82be94e8366cb7f1f474e
Alexandre Feio <alefeio@gmail.com>
2020-02-12 01:33:38 -0300

    implementando websocket para atualização em realtime do sistema web e mobile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 // microframework express
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
+const { setupWebSocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebSocket(server)
 
 mongoose.connect('mongodb+srv://alefeio:al301159@cluster0-qoqjc.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+// porta para acessar o servidor
+server.listen(3333)
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
@@ -22,6 +32,3 @@ Route Params: req.params (identificar um recurso na alteração ou remoção)
 Body: req.body (dados para criação ou alteração de um registro)
 */
 
-
-// porta para acessar o servidor
-app.listen(3333)
